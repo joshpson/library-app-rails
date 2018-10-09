187364adaa3793f3e4d7d6b155336e4821188f91
joshpson <jpearson846@gmail.com>
2018-10-09 17:56:52 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Book.create([
+  { title: "Oathbringer", author: "Brandon Sanderson", condition: "Good", image_url: "https://www.googleapis.com/books/v1/volumes?q=harry+potter"},
+  { title: "Essentialism", author: "Greg McKeown", condition: "Good", image_url: "http://books.google.com/books/content?id=XaGNAQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"}
+])
